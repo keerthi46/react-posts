59a7e7862379e527ed36d41e47367d7b371d1340
Keerthi D M <keerthiraj143.kr@gmail.com>
2022-07-29 18:28:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 import Login from "./pages/Login";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Dashboard from "./pages/Dashboard"
+import { HashRouter, Route, Switch } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Route exact path="/login" component={Login} />
-		<Route exact path="/" component={Dashboard} />
-      </BrowserRouter>
+        <Route exact path="/" component={Dashboard} />
+      </HashRouter>
     </div>
   );
 }
